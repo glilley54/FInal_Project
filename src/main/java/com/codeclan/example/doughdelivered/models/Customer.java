7002ca5f1aa2313a0bb7380b45cd49466e673a16
glilley54 <glilley54@gmail.com>
2021-06-29 16:41:19 +0100

    Customer class basics set

---
+++ src/main/java/com/codeclan/example/doughdelivered/models/Customer.java
@@ -0,0 +1,69 @@
+package com.codeclan.example.doughdelivered.models;
+
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+
+    private Long id;
+
+    private List<Order> orders;
+
+
+    public Customer(String firstName, String lastName, String email, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.address = address;
+        this.orders - new ArrayList<Order>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
