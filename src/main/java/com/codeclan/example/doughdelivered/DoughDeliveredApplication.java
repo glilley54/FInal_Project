24fc2273549afea9691ef30a117b0e6fcd65b796
glilley54 <glilley54@gmail.com>
2021-06-29 16:06:38 +0100

    first commit

---
+++ src/main/java/com/codeclan/example/doughdelivered/DoughDeliveredApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.doughdelivered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoughDeliveredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoughDeliveredApplication.class, args);
+	}
+
+}
