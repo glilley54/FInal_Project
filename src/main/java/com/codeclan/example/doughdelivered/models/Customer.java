78814dcd93432436161a4fc9284b0a5161b0e3db
glilley54 <glilley54@gmail.com>
2021-06-30 11:35:51 +0100

    started defining datbase tables with spring in customer class

--- src/main/java/com/codeclan/example/doughdelivered/models/Customer.java
+++ src/main/java/com/codeclan/example/doughdelivered/models/Customer.java
@@ -1,15 +1,32 @@
 package com.codeclan.example.doughdelivered.models;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Entity
+@Table(name = "customers")
+
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="first_name")
     private String firstName;
+
+    @Column(name ="last_name")
     private String lastName;
+
+    @Column(name= "email")
     private String email;
+
+    @Column(name = "address")
     private String address;
 
-    private Long id;
+
 
     private List<Order> orders;
 
