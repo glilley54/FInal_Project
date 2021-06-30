1ad265ea5a36616b2f17b37012b2547b1e2a880f
glilley54 <glilley54@gmail.com>
2021-06-30 14:11:07 +0100

    updated spring version and application is runing

--- src/test/java/com/codeclan/example/doughdelivered/DoughDeliveredApplicationTests.java
+++ src/test/java/com/codeclan/example/doughdelivered/DoughDeliveredApplicationTests.java
@@ -1,13 +1,30 @@
 package com.codeclan.example.doughdelivered;
-
+import com.codeclan.example.doughdelivered.models.Customer;
+import com.codeclan.example.doughdelivered.repositories.CustomerRepository;
+import com.codeclan.example.doughdelivered.repositories.ItemRepository;
+import com.codeclan.example.doughdelivered.repositories.OrderRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DoughDeliveredApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Autowired
+	OrderRepository orderRepository;
+
+	@Autowired
+	ItemRepository itemRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
+//	@Test
+//	public void customerHasFirstName(){
+//		assertEquals("John", customer.getFirstName);
+//	}
 }
