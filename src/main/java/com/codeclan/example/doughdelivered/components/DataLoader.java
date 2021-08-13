e7c043fc09c922d74e80cac255130c6946c5a954
glilley54 <glilley54@gmail.com>
2021-08-13 16:36:10 +0100

    added text to description

--- src/main/java/com/codeclan/example/doughdelivered/components/DataLoader.java
+++ src/main/java/com/codeclan/example/doughdelivered/components/DataLoader.java
@@ -37,13 +37,13 @@ public class DataLoader implements ApplicationRunner {
         Customer customer2 = new Customer("Brian", "Jones", "brianjones@gmail.com", "25 King Road, London,SW4 5PX");
         customerRepository.save(customer2);
 
-        Item doughballs = new Item("Dough balls", "finest dough in the world",16.00,"https://cdn.shopify.com/s/files/1/0624/9853/products/ooni-frozen-dough-product-studio-0720-1-700x700-2083620_521x521.png?v=1596526089");
+        Item doughballs = new Item("Dough balls", "finest dough in the world. Our dough balls are made to our own 60% hydration recipe, and each dough ball weighs 250 grams, perfect for a 12 pizza. Dough balls will be semi-frozen on arrival",16.00,"https://cdn.shopify.com/s/files/1/0624/9853/products/ooni-frozen-dough-product-studio-0720-1-700x700-2083620_521x521.png?v=1596526089");
         itemRepository.save(doughballs);
 
-        Item sauce = new Item("Tomato Sauce", "finest sauce in the world",7.00,"https://www.cookingclassy.com/wp-content/uploads/2020/05/pizza-sauce-17.jpg");
+        Item sauce = new Item("Tomato Sauce", "finest sauce in the world. It's made with just a few ingredients — canned tomatoes, garlic, balsamic vinegar, and a drizzle of olive oil — and a quick blitz in the blender or food processor",7.00,"https://www.cookingclassy.com/wp-content/uploads/2020/05/pizza-sauce-17.jpg");
         itemRepository.save(sauce);
 
-        Item cheese = new Item("Mozzarella cheese", "finest cheese in the world",08.00,"https://static.toiimg.com/photo/75296834.cms");
+        Item cheese = new Item("Mozzarella cheese", "finest cheese in the world.and kneaded until it  as a main ingredient in pizza.",08.00,"https://static.toiimg.com/photo/75296834.cms");
         itemRepository.save(cheese);
 
         Order order1 = new Order(customer1, "25/06/2021");
